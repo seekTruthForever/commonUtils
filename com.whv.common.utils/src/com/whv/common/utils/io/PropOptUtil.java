e321829bac52bd5c6c17703e0385f606b5102811
seekTruthForever <huaweiwushishui@126.com>
2018-08-15 17:08:09 +0800

    0815

--- com.whv.common.utils/src/com/whv/common/utils/io/PropOptUtil.java
+++ com.whv.common.utils/src/com/whv/common/utils/io/PropOptUtil.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -16,17 +19,91 @@ import java.util.Properties;
  *
  */
 public class PropOptUtil {
+	//默认编码
+		private final static String DEF_CHARSET="UTF-8";
+		private String charset;
+		private InputStream in;
+		public PropOptUtil() {
+			charset = DEF_CHARSET;
+			in = null;
+		}
+		/**
+		 * 通过文件输入流构建工具类构造器
+		 * @param fis 文件输入流
+		 */
+		public PropOptUtil(InputStream fis) {
+			this(fis,null);
+		}
+		/**
+		 * 通过文件输入流构建工具类构造器
+		 * @param fis 文件输入流
+		 * @param encode 编码
+		 */
+		public PropOptUtil(InputStream fis,String encode) {
+			if(encode==null||"".equals(encode)) {
+				charset = DEF_CHARSET;
+			}else {
+				charset = encode;
+			}
+			in = fis;
+		}
+		/**
+		 * 通过文件路径构建工具类构造器
+		 * @param filePath 文件相对路径
+		 */
+		public PropOptUtil(String filePath) {
+			this(filePath,null,true);
+		}
+		/**
+		 * 通过文件路径构建工具类构造器
+		 * @param filePath 文件路径
+		 * @param isRelative 是否相对路径
+		 */
+		public PropOptUtil(String filePath,boolean isRelative) {
+			this(filePath,null,isRelative);
+		}
+		/**
+		 * 通过文件路径构建工具类构造器
+		 * @param filePath 文件相对路径
+		 * @param encode 编码
+		 */
+		public PropOptUtil(String filePath,String encode) {
+			this(filePath,encode,true);
+		}
+		/**
+		 * 通过文件路径构建工具类构造器
+		 * @param filePath 文件路径
+		 * @param encode 编码
+		 * @param isRelative 是否相对路径
+		 */
+		public PropOptUtil(String filePath,String encode,boolean isRelative) {
+			if(encode==null||"".equals(encode)) {
+				charset = DEF_CHARSET;
+			}else {
+				charset = encode;
+			}
+			if(isRelative) {
+				 in = PropOptUtil.class.getClassLoader().getResourceAsStream(filePath);
+			}else {
+				try {
+					in = new FileInputStream(new File(filePath));
+				} catch (FileNotFoundException e) {
+					in = null;
+				}
+			}
+		}
 	/**
 	 * 从属性文件中获取值
 	 * @param filePath 类加载器相对路径
 	 * @return Properties
 	 */
-	public static Properties getProperties(String filePath) {
+	public  Properties getProperties() {
+		if(in==null) {
+			throw new RuntimeException("文件不存在");
+		}
 		Properties properties = new Properties();
-		InputStream in = PropOptUtil.class.getClassLoader()
-				.getResourceAsStream(filePath);
 		try {
-			properties.load(new InputStreamReader(in,"UTF-8"));
+			properties.load(new InputStreamReader(in,charset));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -41,60 +118,12 @@ public class PropOptUtil {
 	}
 	/**
 	 * 从属性文件中获取值
-	 * @param filePath 绝对路径
-	 * @return Properties
-	 */
-	public static Properties getPropertiesAP(String filePath) {
-		Properties properties = new Properties();
-		InputStream in = null;
-		try {
-			in = new FileInputStream(new File(filePath));
-			properties.load(new InputStreamReader(in,"UTF-8"));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (in != null)
-					in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return properties;
-	}
-	/**
-	 * 从属性文件中获取值
-	 * @param filePath 绝对路径
-	 * @param key 键
-	 * @return 值
-	 */
-	public static String getPropertiesValueAP(String filePath,String key) {
-		Properties properties = getPropertiesAP(filePath);
-		String value = properties.getProperty(key).trim();
-		return value;
-	}
-	/**
-	 * 从属性文件中获取值
-	 * @param filePath 绝对路径
-	 * @param key 键
-	 * @param defaultValue 默认值
-	 * @return 值
-	 */
-	public static String getPropertiesValueAP(String filePath,String key,String defaultValue) {
-		Properties properties = getPropertiesAP(filePath);
-		String value = properties.getProperty(key,defaultValue).trim();
-		return value;
-	}
-	/**
-	 * 从属性文件中获取值
 	 * @param filePath 类加载器相对路径
 	 * @param key 键
 	 * @return 值
 	 */
-	public static String getPropertiesValue(String filePath,String key) {
-		Properties properties = getProperties(filePath);
+	public String getPropertiesValue(String key) {
+		Properties properties = getProperties();
 		String value = properties.getProperty(key).trim();
 		return value;
 	}
@@ -105,8 +134,8 @@ public class PropOptUtil {
 	 * @param defaultValue 默认值
 	 * @return 值
 	 */
-	public static String getPropertiesValue(String filePath,String key,String defaultValue) {
-		Properties properties = getProperties(filePath);
+	public String getPropertiesValue(String key,String defaultValue) {
+		Properties properties = getProperties();
 		String value = properties.getProperty(key,defaultValue).trim();
 		return value;
 	}
@@ -117,8 +146,49 @@ public class PropOptUtil {
 	 * @param value 属性值
 	 */
 	public static void createProperties(String filePath,String key,String value){
+		createProperties(filePath, key, value, DEF_CHARSET);
+	}
+	/**
+	 * 写入属性值
+	 * @param filePath 属性文件路径，类加载器相对路径
+	 * @param key 属性名称
+	 * @param value 属性值
+	 */
+	public static void createProperties(String filePath,String key,String value,String charset){
+		createProperties(filePath, key, value, charset,false);
+	}
+	/**
+	 * 写入属性值
+	 * @param filePath 属性文件路径，类加载器相对路径
+	 * @param key 属性名称
+	 * @param value 属性值
+	 */
+	public static void createPropertiesAB(String filePath,String key,String value,String charset){
+		createProperties(filePath, key, value, charset,true);
+	}
+	/**
+	 * 写入属性值
+	 * @param filePath 属性文件路径，类加载器相对路径
+	 * @param key 属性名称
+	 * @param value 属性值
+	 */
+	public static void createPropertiesAB(String filePath,String key,String value){
+		createPropertiesAB(filePath, key, value, DEF_CHARSET);
+	}
+	/**
+	 * 写入属性值
+	 * @param filePath 属性文件路径，类加载器相对路径
+	 * @param key 属性名称
+	 * @param value 属性值
+	 */
+	public static void createProperties(String filePath,String key,String value,String charset,boolean isAbsolute){
 		Properties properties = new Properties();
-		String url = PropOptUtil.class.getClassLoader().getResource(filePath).getPath();
+		String url = null;
+		if(isAbsolute) {
+			url = filePath;
+		}else {
+			url = PropOptUtil.class.getClassLoader().getResource(filePath).getPath();
+		}
 		InputStream in = null;
 		try {
 			File file = new File(url);
@@ -126,10 +196,10 @@ public class PropOptUtil {
 				file.createNewFile();
 			}
 			in = new FileInputStream(file);
-			properties.load(new InputStreamReader(in,"UTF-8"));
+			properties.load(new InputStreamReader(in,charset));
 			properties.setProperty(key, value);
 			FileOutputStream out = new FileOutputStream(file);
-			properties.store(new OutputStreamWriter(out,"UTF-8"), "修改属性文件"+url);
+			properties.store(new OutputStreamWriter(out,charset), "修改属性文件"+url);
 		} catch (IOException e){
 			e.printStackTrace();
 		}finally {
@@ -142,35 +212,34 @@ public class PropOptUtil {
 		}
 		
 	}
-	/**
-	 * 写入属性值
-	 * @param filePath 属性文件路径,绝对路径
-	 * @param key 属性名称
-	 * @param value 属性值
-	 */
-	public static void createPropertiesAp(String filePath,String key,String value){
-		Properties properties = new Properties();
-		InputStream in = null;
-		try {
-			File file = new File(filePath);
-			if(!file.exists()){
-				file.createNewFile();
-			}
-			in = new FileInputStream(file);
-			properties.load(new InputStreamReader(in,"UTF-8"));
-			properties.setProperty(key, value);
-			FileOutputStream out = new FileOutputStream(file);
-			properties.store(new OutputStreamWriter(out,"UTF-8"), "修改属性文件"+filePath);
-		} catch (IOException e){
-			e.printStackTrace();
-		}finally {
-			try {
-				if (in != null)
-					in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
+    /**
+     * 读取属性文件中的属性值
+     * @return
+     */
+    public Map<String,String> readAllProps(){
+        Map<String,String> h = new HashMap<String,String>();
+        try {
+        	Properties props = getProperties();
+            Enumeration<String> er = (Enumeration<String>) props.propertyNames();
+            while (er.hasMoreElements()) {
+                String paramName = er.nextElement();
+                h.put(paramName, props.getProperty(paramName));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return h;
+    }
+	public String getCharset() {
+		return charset;
+	}
+	public void setCharset(String charset) {
+		this.charset = charset;
+	}
+	public InputStream getIn() {
+		return in;
+	}
+	public void setIn(InputStream in) {
+		this.in = in;
 	}
 }
--- com.whv.common.utils/src/com/whv/common/utils/json/JsonUtil.java
+++ com.whv.common.utils/src/com/whv/common/utils/json/JsonUtil.java
@@ -2,12 +2,20 @@ package com.whv.common.utils.json;
 
 import java.io.StringWriter;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.map.ObjectMapper;
-
+import org.codehaus.jackson.type.JavaType;
+/**
+ * json转换工具类
+ * @author whv
+ *
+ */
 public class JsonUtil {
+	//默认编码
+	private final static String DEF_CHARSET="GBK";
     /**
      * JSON data -> json
      * 
@@ -15,7 +23,7 @@ public class JsonUtil {
      * @return
      * @throws Exception
      */
-    public static StringWriter objectToJson(Object data) throws Exception {
+    public static StringWriter object2Json(Object data) throws Exception {
         StringWriter write = new StringWriter();
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -27,7 +35,10 @@ public class JsonUtil {
     }
 
     public static void writeJson(HttpServletResponse rep, Object data) {
-        rep.setContentType("application/json;charset=GBK");
+        writeJson(rep, data, DEF_CHARSET);
+    }
+    public static void writeJson(HttpServletResponse rep, Object data,String charset) {
+        rep.setContentType("application/json;charset="+charset);
         ObjectMapper mapper = new ObjectMapper();
         try {
             mapper.writeValue(rep.getWriter(), data);
@@ -35,9 +46,67 @@ public class JsonUtil {
             e.printStackTrace();
         }
     }
-
+    /**
+     * 将对象转换成json字符串。
+     * <p>Title: pojoToJson</p>
+     * <p>Description: </p>
+     * @param data
+     * @return
+     */
+    public static String object2JsonStr(Object data) {
+    	String str = null;
+    	ObjectMapper mapper = new ObjectMapper();
+    	try {
+			str = mapper.writeValueAsString(data);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+    	return str;
+    }
+    
+    /**
+     * 将json结果集转化为对象
+     * 
+     * @param jsonData json数据
+     * @param clazz 对象中的object类型
+     * @return
+     */
+    public static <T> T json2Pojo(String jsonData, Class<T> beanType) {
+    	ObjectMapper mapper = new ObjectMapper();
+    	T t = null;
+        try {
+            t = mapper.readValue(jsonData, beanType);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        return t;
+    }
+    
+    /**
+     * 将json数据转换成pojo对象list
+     * <p>Title: jsonToList</p>
+     * <p>Description: </p>
+     * @param jsonData
+     * @param beanType
+     * @return
+     */
+    public static <T>List<T> json2List(String jsonData, Class<T> beanType) {
+    	ObjectMapper mapper = new ObjectMapper();
+    	List<T> list = null;
+    	JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, beanType);
+    	try {
+    		list = mapper.readValue(jsonData, javaType);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	return list;
+    }
     public static void appWriteJson(HttpServletResponse rep, Object data,Object collections, int status, String message) {
-        rep.setContentType("application/json;charset=GBK");
+    	appWriteJson(rep, data, collections, status, message, DEF_CHARSET);
+    }
+    public static void appWriteJson(HttpServletResponse rep, Object data,Object collections, int status, String message,String charset) {
+        rep.setContentType("application/json;charset="+charset);
         ObjectMapper mapper = new ObjectMapper();
         try {
         	AppData appData = new AppData();
@@ -49,10 +118,6 @@ public class JsonUtil {
         	appData.setData(data);
         	appData.setCollections(collections);
             mapper.writeValue(rep.getWriter(), appData);
-//            BspInfoImpl bspInfo = new BspInfoImpl();
-//            bspInfo.setUserName("张三");
-//            bspInfo.setUserId("123");
-//            ContextHolder.setBspInfo(bspInfo);
         } catch (Exception e) {
             e.printStackTrace();
         }
